377ef1ee0cfc611ec34160a3c0b866e4d0dd3170
yunities-front <reubenCFormation@gmail.com>
2023-06-25 16:37:06 +0200

    add event_catalogue

---
+++ event-catalogue/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 
+      Chacun de ses composants va correspondre a une route react.
+
+       Cette application consistera des composants suivants
+
+       1) Un composant Header pour la bar de navigation. Si un utilisateur est connecté, nous allons avoir un lien pour voir tous les evenmments,un lien pour creer un evenment et un lien pour se deconnecter. Sinon, nous allons avoir un lien pour se connecter et un lien pour s'incrire
+
+       2) Un composant SignIn pour s'inscrire 
+
+       3) Un composant SignUp pour se connecter 
+
+       4) Un composant Events pour voir les informations de chaque evenment. Nous allons voir le titre, la description et la categoire de l'evnement. Pour chacun des evenments, nous allons aussi avoir un lien pour voir plus d'informations sur l'evenment
+
+       5) Un composant Event qui va afficher des informations d'un evenment que nous aurai choisi de voir. Nous allons avoir le titre, la description et la categorie mais nous allons aussi avour une liste de tous les utilisateurs qui sont inscrit pour cette evenment. Si il reste de la place disponible, nous aurons un bouton pour pouvoir s'inscrire et si notre inscription est reussi, nous allons voir un message de fecilitation. L'inscription sera lié a l'utilisateur connecté et aussi a l'evenement qui est en train d'etre consulté.
+
+       6) Enfin, nous allons aussi avoir un composant pour pouvoir creer son propre evenment ou nous allons preciser le titre,la description et la categorie. L'evenment sera automatiquement attribué a l'utilisateur connecté
+      
+      
+      */}
+    </div>
+  );
+}
+
+export default App;
