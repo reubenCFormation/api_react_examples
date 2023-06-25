12f34ad3cd6b36ee52116a9924577d8c793854c7
yunities-front <reubenCFormation@gmail.com>
2023-06-25 18:16:42 +0200

    add instruction app.js event catalogue

--- event-catalogue/src/App.js
+++ event-catalogue/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
        3) Un composant SignUp pour se connecter 
 
-       4) Un composant Events pour voir les informations de chaque evenment. Nous allons voir le titre, la description et la categoire de l'evnement. Pour chacun des evenments, nous allons aussi avoir un lien pour voir plus d'informations sur l'evenment
+       4) Un composant Events pour voir les informations de chaque evenment. Nous allons voir la date,le titre, la description et la categoire de l'evnement. Pour chacun des evenments, nous allons aussi avoir un lien pour voir plus d'informations sur l'evenment
 
        5) Un composant Event qui va afficher des informations d'un evenment que nous aurai choisi de voir. Nous allons avoir le titre, la description et la categorie mais nous allons aussi avour une liste de tous les utilisateurs qui sont inscrit pour cette evenment. Si il reste de la place disponible, nous aurons un bouton pour pouvoir s'inscrire et si notre inscription est reussi, nous allons voir un message de fecilitation. L'inscription sera lié a l'utilisateur connecté et aussi a l'evenement qui est en train d'etre consulté.
 
